69f85cdb6ae5d9074d5cd02f786696b4fbf922a3
Atul sharma <83007914+Atulsharma428@users.noreply.github.com>
2024-11-13 11:38:46 +0530

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,22 +3,16 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
-import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import nexter from "./tech/nexter.png"
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import postman from "./tech/postman.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import linux from "./tech/linux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 import XAI from "./tech/XAI.jpg";
 import GAN from "./tech/GAN.png";
 import natour from "./tech/natour.png";
@@ -28,11 +22,9 @@ import persistent from "./company/PERSISTENT.NS.png";
 import yolo from "./tech/yolo.webp";
 import trillo2 from "./tech/trillo2.png";
 import python from "./tech/python.png";
-import vercel from "./tech/Vercel.png";
 
 export {
   python,
-  vercel,
   natour,
   GAN,
   nexter,
@@ -43,21 +35,15 @@ export {
   creator,
   mobile,
   web,
-  github,
   menu,
   close,
-  css,
-  docker,
-  postman,
   git,
   html,
   javascript,
-  nodejs,
   reactjs,
   linux,
   tailwind,
   typescript,
-  threejs,
   salesforce,
   Jio,
   persistent,
